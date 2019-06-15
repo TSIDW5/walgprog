aa242ac192e76b327259fc701ed812ddbece6ae9
Diego Marczal <dmarczal@gmail.com>
2019-06-15 16:24:20 -0300

    Update geckodriver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,10 @@
 Capybara.register_driver(:headless_chrome) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu window-size=1280,2000 no-sandbox --remote-debugging-port=9222] }
+    chromeOptions: {
+    #  args: %w[headless disable-gpu window-size=1280,2000 no-sandbox --remote-debugging-port=9222 --enable-features=NetworkService,NetworkServiceInProcess]
+      args: %w[no-sandbox disable-dev-shm-usage disable-popup-blocking headless disable-gpu window-size=1920,1080 --enable-features=NetworkService,NetworkServiceInProcess --disable-features=VizDisplayCompositor],
+        log_level: :error
+    }
   )
 
   Capybara::Selenium::Driver.new(
@@ -10,7 +14,7 @@ Capybara.register_driver(:headless_chrome) do |app|
   )
 end
 
-if ENV['browser.headless'].eql?('true')
+#if ENV['browser.headless'].eql?('true')
   RSpec.configure do |config|
     config.before(:each, type: :system) do
       driven_by :headless_chrome
@@ -18,4 +22,4 @@ if ENV['browser.headless'].eql?('true')
   end
 
   Capybara.javascript_driver = :headless_chrome
-end
+#end
