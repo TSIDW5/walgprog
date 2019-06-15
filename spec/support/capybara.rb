92e5f90650bbec832db926600772d7df9c0abd81
Diego Marczal <dmarczal@gmail.com>
2019-06-15 16:12:02 -0300

    Try spicify chrome version 2

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,6 @@
 Capybara.register_driver(:headless_chrome) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu window-size=1280,2000 no-sandbox] }
+    chromeOptions: { args: %w[headless disable-gpu window-size=1280,2000 no-sandbox --remote-debugging-port=9222] }
   )
 
   Capybara::Selenium::Driver.new(
