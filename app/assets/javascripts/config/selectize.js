41d6b8cc654c50a241d4fc542c01ca0d2fbf9c75
Diego Marczal <dmarczal@gmail.com>
2019-07-17 17:33:03 -0300

    Destroy all selectize before turbolinks cache

--- app/assets/javascripts/config/selectize.js
+++ app/assets/javascripts/config/selectize.js
@@ -2,15 +2,32 @@
 //= require sifter/sifter.min
 //= require selectize/selectize.min
 
+WAlgProg.selectize = {}
+
 $(document).on('turbolinks:load', () => {
-  WAlgProg.selectize();
+  WAlgProg.selectize.init();
 });
 
-WAlgProg.selectize = () => {
+$(document).on('turbolinks:before-cache', () => {
+  WAlgProg.selectize.clearCache();
+});
+
+WAlgProg.selectize.init = () => {
   const selects = $('.apply-selectize');
 
   if (selects.length > 0) {
     selects.selectize();
     $('.selectize-input input[placeholder]').attr('style', 'width: 100%;');
   }
+};
+
+WAlgProg.selectize.clearCache = () => {
+  const selects = $('.selectized');
+  if (selects.length < 1) return;
+
+  selects.each((index, select) => {
+    if (select.selectize) {
+      select.selectize.destroy();
+    }
+  });
 };
